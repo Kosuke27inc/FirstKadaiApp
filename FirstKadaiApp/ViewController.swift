e2f8da3201e316347ee92bc344be2f50657fa53a
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-04-08 22:04:58 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Kosuke Miyazaki on 2024/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
